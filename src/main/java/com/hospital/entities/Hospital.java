62e0a5cb8de7bda7c35b5c92fd2ad5e8b26f214d
Arpine Alvrtsyan <arpine.alvrtsyan@gmail.com>
2019-02-10 17:26:52 +0400

    hospital and patient entities

---
+++ src/main/java/com/hospital/entities/Hospital.java
@@ -0,0 +1,61 @@
+package com.hospital.entities;
+
+import java.util.List;
+
+public class Hospital {
+
+    private int id;
+    private String name;
+    private String username;
+    private String password;
+    private List<Doctor> doctors;
+    private List<String> services;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Doctor> getDoctors() {
+        return doctors;
+    }
+
+    public void setDoctors(List<Doctor> doctors) {
+        this.doctors = doctors;
+    }
+
+    public List<String> getServices() {
+        return services;
+    }
+
+    public void setServices(List<String> services) {
+        this.services = services;
+    }
+}
