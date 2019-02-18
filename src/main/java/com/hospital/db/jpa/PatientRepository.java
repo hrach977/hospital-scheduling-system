3bbb98f1c288ba888a48308218e4079bc0894719
Arpine Alvrtsyan <arpine.alvrtsyan@gmail.com>
2019-02-18 14:02:31 +0400

    PatientSignUp

--- src/main/java/com/hospital/db/jpa/PatientRepository.java
+++ src/main/java/com/hospital/db/jpa/PatientRepository.java
@@ -3,5 +3,9 @@ package com.hospital.db.jpa;
 import com.hospital.entities.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PatientRepository extends JpaRepository<Patient, Long> {
+    List<Patient> findByUserName(String username);
+    Patient save(Patient patient);
 }
--- src/main/java/com/hospital/service/PatientSignUpService.java
+++ src/main/java/com/hospital/service/PatientSignUpService.java
@@ -1,4 +1,41 @@
 package com.hospital.service;
 
-public class PatientSignUpService {
-}
+import com.hospital.db.jpa.PatientRepository;
+import com.hospital.entities.Patient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+    @Service
+    public class PatientSignUpService {
+
+        private static final Logger logger = LoggerFactory.getLogger(PatientSignUpService.class);
+
+        @Autowired
+        PatientRepository patientRepository;
+
+        @Transactional
+        public boolean signUp(String firstName, String lastName, String userName, String email, String password, String passwordConfirmation){
+            logger.debug("signUp service");
+            if(patientRepository.findByUserName(userName) != null){
+                logger.error("This username already in use!");
+                return false;
+            }
+            else if(password != passwordConfirmation){
+                logger.error("Password does not match");
+                return false;
+            }
+            else{
+                Patient patient = new Patient(firstName,lastName,userName,email,password);
+                logger.info("You have successfully registered.");
+                patientRepository.save(patient);
+
+                return true;
+            }
+        }
+
+    }
+
+
