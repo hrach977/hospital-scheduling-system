be180fc23ea5bdd9e0f62a6194856c29ff808b68
Arpine Alvrtsyan <arpine.alvrtsyan@gmail.com>
2019-02-22 00:58:29 +0400

    renaming

--- src/main/java/com/hospital/controller/PatientController.java
+++ src/main/java/com/hospital/controller/PatientController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class Patient_controller {
+public class PatientController {
 
     @Autowired
     @Qualifier("internal")
