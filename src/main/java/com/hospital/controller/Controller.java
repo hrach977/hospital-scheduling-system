e64a9bfa10afd82601aba79a2454285d28ae4da0
hrach977 <hrachya.yeghishyan@picsart.com>
2019-02-21 23:01:24 +0400

    switch to internal interface (patients)

--- src/main/java/com/hospital/controller/Controller.java
+++ src/main/java/com/hospital/controller/Controller.java
@@ -1,8 +1,9 @@
 package com.hospital.controller;
 
-import com.hospital.db.jpa.PatientRepository;
+import com.hospital.db.PatientRepository;
 import com.hospital.entities.Patient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,14 +11,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Controller {
 
+//    @Autowired
+//    PatientRepository patientRepository;
+
     @Autowired
+    @Qualifier("internal")
     PatientRepository patientRepository;
 
     @PostMapping(value = "person/test")
     public String indexPatient() {
         System.out.println("creating the patient");
         Patient patient = new Patient();
-        patient.setUserName("urishHivand");
+        patient.setUserName("MerInterfaceov");
         patient.setFirstName("pacient");
         patient.setEmail("hivand@gmail.com");
         patient.setPassword("qwerty");
